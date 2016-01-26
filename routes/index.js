db57f4b8d3cc88957bb89c6b22af1e17b7495f2c
Matt White <matt@ldcvia.com>
2016-01-26 11:15:28 +0000

    Add static about and contact pages with content stored in back end. Tidied up login page

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,39 @@ router.get('/', auth.requiresLogin,  function(req, res, next) {
       }
     )
     .on('complete', function(data, response){
-      res.render('index', {"tab":"home", "ideas": data});
+      res.render('index', {"tab":"home", "email": req.cookies.email, "ideas": data});
+    });
+  }catch(e){
+    res.render("login", {"error": e});
+  }
+});
+
+router.get('/about', function(req, res, next) {
+  try{
+    restler.get(
+      process.env.LDCVIA_IDEAS_APIHOST + "/document/ldcvia-ideas/staticpages/about",
+      {headers:
+        {'apikey': process.env.LDCVIA_IDEAS_ADMINAPIKEY}
+      }
+    )
+    .on('complete', function(data, response){
+      res.render('static', {"tab":"about", "page": data});
+    });
+  }catch(e){
+    res.render("login", {"error": e});
+  }
+});
+
+router.get('/contact', function(req, res, next) {
+  try{
+    restler.get(
+      process.env.LDCVIA_IDEAS_APIHOST + "/document/ldcvia-ideas/staticpages/contact",
+      {headers:
+        {'apikey': process.env.LDCVIA_IDEAS_ADMINAPIKEY}
+      }
+    )
+    .on('complete', function(data, response){
+      res.render('static', {"tab":"contact", "page": data});
     });
   }catch(e){
     res.render("login", {"error": e});
@@ -22,7 +54,7 @@ router.get('/', auth.requiresLogin,  function(req, res, next) {
 
 /* GET login page */
 router.get('/login',  function(req, res, next) {
-  res.render('login');
+  res.render('login', {"tab": "home"});
 });
 
 router.post('/login', function(req, res, next){
